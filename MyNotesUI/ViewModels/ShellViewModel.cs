bf65cd078d93c5cf80766485b8bc034e327e8216
usamah <usamdsb92@gmail.com>
2023-03-25 13:45:58 +0800

    Add WPF Front End and Class Library
    Adds WPF with MVVM using Caliburn.Micro and class library project type

---
+++ MyNotesUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotesUI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        public bool CanClear(string title, string note)
+        {
+            if (String.IsNullOrWhiteSpace(title) || String.IsNullOrWhiteSpace(note))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public void Clear(string title, string note)
+        {
+            TitleText = "";
+            NoteText = "";
+        }
+    }
+}
